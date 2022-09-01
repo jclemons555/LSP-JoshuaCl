8e9ad5c3b7f3881858cb72da7e0108b79d1b9d17
joshuaclemons <joshuaclemons@localhost>
2022-09-01 19:07:38 -0400

    commit for assignment 1

---
+++ large-scale-programming-assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Joshua Clemons
+ * @author joshuaclemons
+ * 
+ * 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
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+
+	}
+
+}
