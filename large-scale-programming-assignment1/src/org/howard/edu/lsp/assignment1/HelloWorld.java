86e6e2ee5050265c84d550b51550acdc2bbde64d
joshuaclemons <joshuaclemons@localhost>
2022-09-01 19:50:28 -0400

    commit assignment 1

--- large-scale-programming-assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ large-scale-programming-assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -3,10 +3,6 @@ package org.howard.edu.lsp.assignment1;
  * Joshua Clemons
  * @author joshuaclemons
  * 
-<<<<<<< HEAD
- * 
-=======
->>>>>>> branch 'main' of https://github.com/jclemons555/LSP_-jclemons555.git
  */
 public class HelloWorld {
 
