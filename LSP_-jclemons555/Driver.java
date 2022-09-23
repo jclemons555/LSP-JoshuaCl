6f2aef70277cb4d9d1875a3b2ef66cf32411e043
jclemons555 <83421930+jclemons555@users.noreply.github.com>
2022-09-22 22:15:39 -0400

    Update Driver.java

--- LSP_-jclemons555/Driver.java
+++ LSP_-jclemons555/Driver.java
@@ -8,8 +8,9 @@ public class Driver {
 		System.out.println(comb.compute(new int[]{5,5,15,10}, 15));
 		System.out.println(comb.compute(new int[]{1,2,3,4,5}, 16));
 		System.out.println(comb.compute(new int[]{1,2,3,4,5,6}, 8));
-		System.out.println(comb.compute(new int[]{5,15,10,3,7,8,2}, 15));
 		System.out.println(comb.compute(new int[]{12,3,2,5,7,9,8,11}, 11));
+		System.out.println(comb.compute(new int[]{1,2,3,4,5}, 13));
+		System.out.println(comb.compute(new int[]{}, 15));
 		
 	}
 
