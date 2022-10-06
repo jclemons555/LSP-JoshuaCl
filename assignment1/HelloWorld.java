ea26259a287b9190b3bfff2d89980b09d246d859
jclemons555 <83421930+jclemons555@users.noreply.github.com>
2022-10-06 12:20:24 -0400

    Add files via upload

---
+++ assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Joshua Clemons
+ * @author joshuaclemons
+ * 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+
+	}
+
+}
