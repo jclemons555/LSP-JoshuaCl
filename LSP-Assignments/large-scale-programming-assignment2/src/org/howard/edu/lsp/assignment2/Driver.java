e9a58f01c31ec1bd99cbc06f1708c3ec9fd21bfa
jclemons555 <83421930+jclemons555@users.noreply.github.com>
2022-10-06 17:21:15 -0400

    Add files via upload

---
+++ LSP-Assignments/large-scale-programming-assignment2/src/org/howard/edu/lsp/assignment2/Driver.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment2;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Combinations comb = new Combinations();
+		
+		System.out.println(comb.compute(new int[]{5,5,15,10}, 15));
+		System.out.println(comb.compute(new int[]{1,2,3,4,5}, 16));
+		System.out.println(comb.compute(new int[]{1,2,3,4,5,6}, 8));
+		System.out.println(comb.compute(new int[]{12,3,2,5,7,9,8,11}, 11));
+		System.out.println(comb.compute(new int[]{1,2,3,4,5}, 13));
+		System.out.println(comb.compute(new int[]{}, 15));
+		
+	}
+
+}
