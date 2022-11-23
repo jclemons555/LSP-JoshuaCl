9352b9601c21711ff2ccae1028b148e53d791739
jclemons555 <83421930+jclemons555@users.noreply.github.com>
2022-11-23 15:31:48 -0500

    Create AverageStrategy.java

---
+++ LSP-Assignments/large-scale-programming/src/org/howard/edu/lsp/assignment7/AverageStrategy.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignment7;
+
+import java.util.List;
+
+public interface AverageStrategy {
+
+	public int compute(List<Integer> grades) throws EmptyListException;
+
+	
+
+}
